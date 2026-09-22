31bd6c449da42ec6badbdcf0982f91d25d9364b7
harshpatil1999 <patilh652@gmail.com>
2026-09-22 23:55:21 +0530

    wrapping the GoogleOAuthProvider component to inject Google OAuth Client ID into the frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AppProvider } from "./context/AppContext.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export const server = "http://localhost:5000";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProvider>
-      <App />
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        {" "}
+        <App />
+      </GoogleOAuthProvider>
     </AppProvider>
   </StrictMode>,
 );
